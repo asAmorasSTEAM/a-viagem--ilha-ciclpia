2bef9df3025b91405cdf019e2eb42cd7b29301e6
asAmorasSTEAM <a1590@escolaglobal.org>
2026-05-22 16:32:35 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,188 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . f f . . . . . . 
+. . . . f f f f . . . . . 
+. f f f f f f f f f f . . 
+. f f f f f f f f f f . . 
+6 6 6 6 6 6 6 6 6 6 6 6 . 
+9 f f f f f d d f f f 9 . 
+f f f f f d d f f f f f . 
+f f f 9 f d d f 9 f f f . 
+. f d 1 f d d f 1 d f . . 
+. f d d d d d d d d f . . 
+. f f f d d d d f f f . . 
+f d f 9 6 6 6 6 9 f d f . 
+d d f 6 6 6 6 6 6 f d d . 
+d d f 9 9 9 9 9 9 f d d . 
+. . . 6 6 6 6 6 6 . . . . 
+. . . e e . . e e . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . f f f f f . . . 
+. . . f f f f f f f f f . 
+. . f f f f f f f f f f . 
+. . f f f f f f f f f f f 
+6 6 6 6 6 6 6 6 6 6 6 6 6 
+f f f f f d f f f f f 9 9 
+. f f f d d f f f f f f 9 
+. . f f d d f 9 f d d f f 
+. . . f d d f 1 d d d f . 
+. . . f d d d d d f f f . 
+. . . f f d d d d d f . . 
+. . . f 6 6 6 6 6 6 6 . . 
+. . . f 9 9 9 9 9 9 9 . . 
+. . . f 6 6 6 6 6 6 f . . 
+. . . . f f f f f f . . . 
+. . . . . . e e e . . . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . f f f f f . . . . . 
+. f f f f f f f f f . . . 
+. f f f f f f f f f f . . 
+f f f f f f f f f f f . . 
+6 6 6 6 6 6 6 6 6 6 6 6 6 
+9 9 f f f f f d f f f f f 
+9 f f f f f f d d f f f . 
+f f d d f 9 f d d f f . . 
+. f d d d 1 d d d f . . . 
+. f f f d d d d d f . . . 
+. . f d d d d d d f . . . 
+. . 6 6 6 6 6 6 6 f . . . 
+. . 9 9 9 9 9 9 9 f . . . 
+. . f 6 6 6 6 6 6 f . . . 
+. . . f f f f f f . . . . 
+. . . . e e e . . . . . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . . 4 4 4 4 4 4 4 4 . . . . 
+. . . 4 4 6 6 6 6 6 6 4 4 . . . 
+. . . 4 4 6 f f f f 6 4 4 . . . 
+. . . 4 4 6 f f f f 6 4 4 . . . 
+4 4 4 4 4 6 f f f f 6 4 4 4 4 4 
+4 4 4 4 4 6 6 6 6 6 6 4 4 4 4 4 
+4 . . 4 f f 1 f f 1 f f 4 . . 4 
+4 . . 4 f 2 1 2 2 1 2 f 4 . . 4 
+4 . . 4 f 2 2 2 2 2 2 f 4 . . 4 
+4 . . 4 2 4 2 2 2 2 4 2 4 . . 4 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . 4 4 4 4 4 4 4 4 4 4 . . . 
+. . . . 4 4 4 4 . 4 4 4 4 . . . 
+. . . . 4 . 4 . . 4 . 4 . . . . 
+`;
+            case "image5":
+            case "myImage3":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . 1 1 1 1 1 1 1 1 . . . . 
+. . . . 1 1 1 1 1 1 1 1 . . . . 
+. . 3 3 d d d d d d d d 3 3 . . 
+. . 3 3 d d d d d d d d 3 3 . . 
+. . . . d f 9 d d f 9 d . . . . 
+. . . . d f 1 d d f 1 d . . . . 
+. . . 1 d d d d d d d d 1 . . . 
+. . 1 1 d d 2 2 2 2 d d 1 1 . . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. 1 1 1 1 1 1 1 1 1 1 1 1 1 1 . 
+. . 1 1 1 1 1 1 1 1 1 1 1 1 . . 
+. . . f f f . . . . f f f . . . 
+`;
+            case "image6":
+            case "myImage4":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+e e e e e e e e e e e e e e e e 
+e e e e e e e e e e e e e e e . 
+e e e e e e e e e e e e e e . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "myImage5":return img`
+....................
+....................
+....................
+....................
+......ffff..........
+....ffffffff........
+...ffffffffff.......
+..ffffffffffff......
+..888888888888......
+..ffffffeeffff......
+..fffffeefffff......
+..fff6feef6fff......
+...fe1feef1ef.......
+...feeeeeeeef.......
+...fffeeeefff.......
+..fef899998fef......
+..eef999999fee......
+..eef666666fee......
+.....888888.........
+.....88..88.........
+`;
+            case "image8":
+            case "myImage6":return img`
+. . . . . f f f f f . . . . . . 
+. . . f f f f f f f f f . . . . 
+. . f f f f f f f f f f . . . . 
+. . f f f f f f f f f f f . . . 
+f 8 8 8 8 8 8 8 8 8 8 8 8 . . . 
+f f f f f e f f f f f f f . . . 
+. f f f e e f f f f f f f . . . 
+. . f f e e f 6 f e e f f . . . 
+. . . f e e f 1 e d e f . . . . 
+. . . f e e e e e f f f . . . . 
+. . . f e e e e e e f . . . . . 
+. . . f 8 9 9 9 9 8 f . . . . . 
+. . . f 9 9 9 9 9 9 f . . . . . 
+. . . f 6 6 f 6 6 6 f . . . . . 
+. . . . f f 8 8 8 f . . . . . . 
+. . . . . . f f f . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
