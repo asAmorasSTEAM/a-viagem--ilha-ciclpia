10ab707cd0ef0fb9291b385f384684f034ec6725
asAmorasSTEAM <a1590@escolaglobal.org>
2026-06-05 16:48:19 +0100

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -21,7 +21,32 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile9 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile10 = image.ofBuffer(hex``);
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level":
+            case "level1":return tiles.createTilemap(hex`5000080004040101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010404010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101050505010101010101010101010104010101010101010101010101010101010101010101010101010101010501010105010101010101010101010101010101010101010101010105010101010101010105050501010101010101010104040404040401010101010505050505010101010101010101010101010105040505050405050101010101010101010101010101010101010101050405010101010101010101050505010101010101040404040404040401010101050505050501010101010101010101010101010504050505040404050101010101010101010105010101050101010504040405010101010101010105050505010101040404040404040404040401010101010101010101030101030101010505010105050404040404040404050606060606060606050505050505050505040404040405050501010101010101010504040404040404040404040404040401010101010101010101030301010505040405050505040404040404040404050505050505050504040404040404040404040404040404040505050505050505050404040404040404040404040404040505050505050505040202020202`, img`
+................................................................................
+................................................................................
+.............................................222...........2....................
+........2...2.......................2........222.........222222.....22222.......
+.......22222222....................222.........222......22222222....22222.......
+.......222222222..........2...2...22222........2222...22222222222..........2..2.
+..22..222222222222222222222222222222222222........2222222222222222..........22..
+222222222222222222222222222222222222222222222222222222222222222222222222222.....
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile9,sprites.dungeon.stairLadder], TileScale.Sixteen);
+        }
+        return null;
+    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -44,8 +69,16 @@ namespace myTiles {
             case "tile8":return tile8;
             case "myTile7":
             case "tile9":return tile9;
-            case "myTile8":
-            case "tile10":return tile10;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile15":
+            case "tile17":return tile17;
         }
         return null;
     })
